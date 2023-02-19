4e0e561a23388978a3c76f21e41f92b9a863c7ab
AmeMeida <ame.pistache@gmail.com>
2023-02-19 11:58:08 -0300

    primeira versao ok do site

--- src/App.svelte
+++ src/App.svelte
@@ -6,12 +6,31 @@
 
   let correctDate: boolean;
   isDate.subscribe((isDate) => correctDate = isDate);
+
+  let bypass: boolean = false;
 </script>
 
 <main>
-  {#if correctDate}
-    <Countdown />
-  {:else}
+  {#if correctDate || bypass}
     <Video />
+  {:else}
+    <Countdown />
+    <button on:click={() => bypass = true}>gi por favor nao aperte aqui</button>
   {/if}
 </main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+  }
+
+  button {
+    position: absolute;
+    bottom: 1rem;
+  }
+</style>
